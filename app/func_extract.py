9a297b6fd2856b413129916bfbd0bf8ed9f76d19
Aaron Portnoy <aportnoy@gmail.com>
2024-12-05 12:09:49 -0500

    enhancements

--- app/func_extract.py
+++ app/func_extract.py
@@ -1,41 +1,75 @@
 import argparse
 import os
 import subprocess
+import base64
 import glob
+import json
 from pathlib import Path
 import pdb
 import r2pipe
 
 def main(args):
     #Iterate over all directories in input_path
-    for directory in glob.glob(f"{args.input_path}/*"):
-
+    if True:
         #Iterate over all binary files
-        for binary in glob.glob(f"{directory}/*"):
+        for binary in glob.glob(f"{args.input_path}/*"):
+            print(binary)
             #radare analyse the app and enumerate all functions
             r2 = r2pipe.open(binary)
             r2.cmd('aa')
-            functions = r2.cmd('afl')
-            functions = functions.splitlines()
+            functions = r2.cmd('afl').splitlines()
+
+
+            func_info = {}
             disasses = {}
 
             #For each function
             #Extract disassembly and write file in output path
             for each_function in functions:
+                parts = each_function.split()  # Split function details
+                
+                name = parts[3]
+                size = int(parts[2])
+                start_addr = int(parts[0], 16)
+
+                # skip import stubs
+                if ".imp." in name: 
+                    continue
+
+                print(f"Processing {name}")
+                
+                raw_bytes = r2.cmd(f"p8 {size} @ {start_addr}").strip()
+                if not raw_bytes:
+                    print(f"No raw bytes retrieved for {name}")
+                    continue
+                
+                raw_bytes = bytes.fromhex(raw_bytes)
+                raw_bytes = base64.b64encode(raw_bytes).decode('utf-8')
+            
                 each_function = each_function.split()[-1]
-                disass = r2.cmd(f'pdf @ {each_function}')
-                disasses[each_function] = disass
+                disass = r2.cmd(f'pdf @ {each_function}').strip()
+                disass = bytes(disass.encode())
+                disass = base64.b64encode(disass).decode('utf-8')
 
-
-            if len(disasses) > 0:
+                func_info[start_addr] = { \
+                    'name': name,
+                    'start': start_addr,
+                    'end': start_addr + size,
+                    'bytes': raw_bytes,
+                    'disassembly': disass
+                }
+                    
+            if len(func_info) != 0:
                 output_dir = binary.replace(args.input_path, args.output_path)
                 dir_path = Path(output_dir)
                 dir_path.mkdir(parents=True, exist_ok=True)
 
-            for func in disasses:
+            for func_addr, values in func_info.items():
                 try:
-                    with open(os.path.join(dir_path, func), 'w') as fi_le:
-                        fi_le.write(disasses[func])
+                    name = values['name']
+                    with open(os.path.join(dir_path, f"{name}.json"), 'w') as fh:
+                        obj = {func_addr: values}
+                        json.dump(obj, fh, indent=4)
                 except Exception as e:
                     print(e)
 
